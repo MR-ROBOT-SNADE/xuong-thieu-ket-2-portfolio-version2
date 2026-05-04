1d90def13bbb59942971f8b3924895229f23639a
MR-ROBOT-SNADE <tungbui911@gmail.com>
2026-05-04 21:32:00 +0700

    Add files via upload

--- js/api/api_config.js
+++ js/api/api_config.js
@@ -1,8 +1,17 @@
 /* CẤU HÌNH API DỮ LIỆU */
 
-const API_KEY_TK3 = 'https://script.google.com/macros/s/AKfycbxl2vmeyXEtNXXCsL__sAkOGJ7QqjAKLfUxlmz5h4eDZ4f8Jhs3R7PIg8jCHwUEcrS0Rg/exec';
-const API_KEY_TK4 = 'https://script.google.com/macros/s/AKfycbw8HoojYT17qeoBCryE1yYi4_W5ThpRx5KQPp3DgSN3sRinKjP3qi4uupZE8b0oA4qN/exec';
+let API_KEY_TK3 = '';
+let API_KEY_TK4 = '';
 
+if (window.location.hostname.includes('netlify.app')) {
+    // Đang sử dụng netlify, phải redirect api để lách luồng tường lửa của công ty
+    API_KEY_TK3 = '/api/tk3';
+    API_KEY_TK4 = '/api/tk4';
+} else {
+    // Nếu là GitHub Pages hoặc chạy ở Local (máy tính ở nhà) -> Gọi thẳng link gốc
+    API_KEY_TK3 = 'https://script.google.com/macros/s/AKfycbxl2vmeyXEtNXXCsL__sAkOGJ7QqjAKLfUxlmz5h4eDZ4f8Jhs3R7PIg8jCHwUEcrS0Rg/exec';
+    API_KEY_TK4 = 'https://script.google.com/macros/s/AKfycbw8HoojYT17qeoBCryE1yYi4_W5ThpRx5KQPp3DgSN3sRinKjP3qi4uupZE8b0oA4qN/exec';
+}
 
 const SHEET_ID_TK3 = '643230965';
 const SHEET_ID_TK4 = '1040221561';
